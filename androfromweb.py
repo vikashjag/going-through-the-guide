b9f9afcf1aef98daad1b096d0df652a764484ff0
Andro <andro@Andros-MacBook-Pro.local>
2020-04-01 13:46:36 +0300

    deleted waste

--- androfromweb.py
+++ androfromweb.py
@@ -1,14 +1,1 @@
-code and more code
-ok adding on the atom lets see. more and more fun stuff
-
-self.assert_(boolean expression, 'message')
-
-self.assertFalse(, 'message')
-
-self.assertIs(expected, actual, 'message')
-
-
-added more text
-
-
-fin
+print("hello world")
