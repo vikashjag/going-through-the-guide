5591eb09304efb0566627bf8b548531559d00c48
Andro <andro@Andros-MacBook-Pro.local>
2020-04-01 12:46:24 +0300

    more added

--- androfromweb.py
+++ androfromweb.py
@@ -1,1 +1,11 @@
 code and more code
+ok adding on the atom lets see. more and more fun stuff
+
+self.assert_(boolean expression, 'message')
+
+self.assertFalse(, 'message')
+
+self.assertIs(expected, actual, 'message')
+
+
+added more text
